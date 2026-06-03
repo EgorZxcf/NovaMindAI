2dea6f30488e067642f80882ea280ee7d0d0505f
EgorZxcf <uduakegor@gmail.com>
2026-06-04 00:02:32 +0300

    Add vision AI

--- app.py
+++ app.py
@@ -2,34 +2,105 @@ import os
 import base64
 import requests
 
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI
+from fastapi import UploadFile
+from fastapi import File
+from fastapi import Form
+
 from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+API_KEY = os.getenv(
+    "OPENROUTER_API_KEY"
+)
+
 @app.get("/")
 def home():
-    return FileResponse("index.html")
+    return FileResponse(
+        "index.html"
+    )
 
 @app.get("/style.css")
 def style():
-    return FileResponse("style.css")
+    return FileResponse(
+        "style.css"
+    )
 
 @app.get("/script.js")
 def script():
-    return FileResponse("script.js")
+    return FileResponse(
+        "script.js"
+    )
 
-@app.post("/upload_image")
-async def upload_image(
-    image: UploadFile = File(...)
+@app.post("/analyze_image")
+async def analyze_image(
+
+    image: UploadFile = File(...),
+
+    question: str = Form(
+        "Что изображено на картинке?"
+    )
+
 ):
 
-    content = await image.read()
+    image_bytes =
+    await image.read()
 
-    encoded = base64.b64encode(
-        content
+    image_base64 =
+    base64.b64encode(
+        image_bytes
     ).decode()
 
+    response =
+    requests.post(
+
+        "https://openrouter.ai/api/v1/chat/completions",
+
+        headers={
+            "Authorization":
+            f"Bearer {API_KEY}",
+            "Content-Type":
+            "application/json"
+        },
+
+        json={
+
+            "model":
+            "google/gemini-2.5-flash-image-preview",
+
+            "messages":[
+                {
+                    "role":"user",
+                    "content":[
+
+                        {
+                            "type":"text",
+                            "text":question
+                        },
+
+                        {
+                            "type":"image_url",
+                            "image_url":{
+                                "url":
+                                f"data:image/jpeg;base64,{image_base64}"
+                            }
+                        }
+
+                    ]
+                }
+            ]
+        },
+
+        timeout=120
+    )
+
+    result =
+    response.json()
+
+    answer =
+    result["choices"][0]["message"]["content"]
+
     return {
-        "image": encoded
+        "reply":answer
     }
